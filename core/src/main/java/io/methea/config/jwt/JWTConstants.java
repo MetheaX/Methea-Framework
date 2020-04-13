3884a3a88ea904c0f9be6ef45f702ff10b3f2150
DKSilverX <limit.tith@outlook.com>
2020-04-13 12:44:57 +0700

    Add defualt JWT properties

---
+++ core/src/main/java/io/methea/config/jwt/JWTConstants.java
@@ -0,0 +1,13 @@
+package io.methea.config.jwt;
+
+/**
+ * Author : DKSilverX
+ * Date : 13/04/2020
+ */
+public class JWTConstants {
+    private JWTConstants(){}
+    static final String SECRET = "S'jG,i:bM3'ol}Di4s@7&O=r}x+Rs[[#&Fhj8{X_5^v}*CftMIE4ZwFX3z0XN]{";
+    static final String EXPIRATION_TIME = "8600000"; // 1 days
+    static final String TOKEN_PREFIX = "Bearer";
+    static final String HEADER_STRING = "Authorization";
+}
