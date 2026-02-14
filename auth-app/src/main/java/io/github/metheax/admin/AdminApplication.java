3cd417b075ff6f554be90f18f13ac666e53bac39
Kuylim TITH <kuylim.tith@gmail.com>
2026-02-14 10:48:50 -0800

    [Migration] - jdk 17 to jdk 21 & spring boot 3 to spring boot 4

--- auth-app/src/main/java/io/github/metheax/admin/AdminApplication.java
+++ auth-app/src/main/java/io/github/metheax/admin/AdminApplication.java
@@ -2,7 +2,7 @@ package io.github.metheax.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
